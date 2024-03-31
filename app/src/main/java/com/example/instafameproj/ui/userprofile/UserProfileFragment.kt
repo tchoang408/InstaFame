d30d58ffe310f3e274919043045ae2f71d05b0d2
Damian Ly <lydamian@gmail.com>
2024-03-30 19:31:44 -0700

    working stage added email and user_id text fields to the UI

--- app/src/main/java/com/example/instafameproj/ui/userprofile/UserProfileFragment.kt
+++ app/src/main/java/com/example/instafameproj/ui/userprofile/UserProfileFragment.kt
@@ -8,6 +8,7 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.instafameproj.databinding.FragmentNotificationsBinding
+import androidx.lifecycle.Observer
 
 class UserProfileFragment : Fragment() {
 
@@ -22,18 +23,33 @@ class UserProfileFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val notificationsViewModel =
+        val userProfileViewModel =
             ViewModelProvider(this).get(UserProfileViewModel::class.java)
+
+        userProfileViewModel.updateUserData(
+            "lydamian@gmail.com",
+            "23oifajsd;fkj20392r30000"
+        )
 
         _binding = FragmentNotificationsBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
         val textView: TextView = binding.textNotifications
-        notificationsViewModel.text.observe(viewLifecycleOwner) {
+        userProfileViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
+
+        // Observe LiveData for user data
+        userProfileViewModel.userEmail.observe(viewLifecycleOwner, Observer { email ->
+            binding.email.text = email
+        })
+
+        userProfileViewModel.userId.observe(viewLifecycleOwner, Observer { userId ->
+            binding.userId.text = userId
+        })
         return root
     }
+
 
     override fun onDestroyView() {
         super.onDestroyView()
--- app/src/main/java/com/example/instafameproj/ui/userprofile/UserProfileViewModel.kt
+++ app/src/main/java/com/example/instafameproj/ui/userprofile/UserProfileViewModel.kt
@@ -4,10 +4,29 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
+
 class UserProfileViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
         value = "This is User Profile Fragment"
     }
+
+    // LiveData objects to hold user data
+    private val _userEmail = MutableLiveData<String>()
+    private val _userId = MutableLiveData<String>()
+
     val text: LiveData<String> = _text
+
+    // Expose LiveData as read-only properties
+    val userEmail: LiveData<String>
+        get() = _userEmail
+    val userId: LiveData<String>
+        get() = _userId
+
+    // Function to update user data (call from your activity)
+    fun updateUserData(email: String, userId: String) {
+        _userEmail.value = email
+        _userId.value = userId
+    }
+
 }
