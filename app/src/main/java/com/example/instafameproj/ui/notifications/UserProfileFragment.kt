58cd3e29b53c1037ffd5520f6cdb5825a5ff92bf
Damian Ly <lydamian@gmail.com>
2024-03-30 18:17:47 -0700

    Renamed the Notifications bottom naigation bar button to User Profile

--- app/src/main/java/com/example/instafameproj/ui/notifications/UserProfileFragment.kt
+++ app/src/main/java/com/example/instafameproj/ui/notifications/UserProfileFragment.kt
@@ -9,7 +9,7 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.instafameproj.databinding.FragmentNotificationsBinding
 
-class NotificationsFragment : Fragment() {
+class UserProfileFragment : Fragment() {
 
     private var _binding: FragmentNotificationsBinding? = null
 
