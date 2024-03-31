cf19f3fe98cdb8177bbe82f94d92fb768ddccdc0
Tam Hoang <tamhoang408@yahoo.com>
2024-03-30 18:28:20 -0700

    update xml file name and text

--- app/src/main/java/com/example/instafameproj/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/example/instafameproj/ui/dashboard/DashboardFragment.kt
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.example.instafameproj.databinding.FragmentDashboardBinding
+import com.example.instafameproj.databinding.FragmentHomeBinding
 
 class DashboardFragment : Fragment() {
 
-    private var _binding: FragmentDashboardBinding? = null
+    private var _binding: FragmentHomeBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -25,10 +25,10 @@ class DashboardFragment : Fragment() {
         val dashboardViewModel =
             ViewModelProvider(this).get(DashboardViewModel::class.java)
 
-        _binding = FragmentDashboardBinding.inflate(inflater, container, false)
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textDashboard
+        val textView: TextView = binding.textHome
         dashboardViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
