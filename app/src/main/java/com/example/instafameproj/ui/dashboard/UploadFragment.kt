e045d3852120c93496bb99ba2f6a46048bae4789
Tam Hoang <tamhoang@yahoo.com>
2024-04-04 00:07:59 -0700

    refactor file name and add authentication class

--- app/src/main/java/com/example/instafameproj/ui/dashboard/UploadFragment.kt
+++ app/src/main/java/com/example/instafameproj/ui/dashboard/UploadFragment.kt
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.example.instafameproj.databinding.FragmentHomeBinding
+import com.example.instafameproj.databinding.FragmentUploadBinding
 
-class DashboardFragment : Fragment() {
+class UploadFragment : Fragment() {
 
-    private var _binding: FragmentHomeBinding? = null
+    private var _binding: FragmentUploadBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -23,12 +23,12 @@ class DashboardFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         val dashboardViewModel =
-            ViewModelProvider(this).get(DashboardViewModel::class.java)
+            ViewModelProvider(this).get(UploadViewModel::class.java)
 
-        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        _binding = FragmentUploadBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textHome
+        val textView: TextView = binding.uploadTV
         dashboardViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
--- app/src/main/java/com/example/instafameproj/ui/userprofile/UserProfileFragment.kt
+++ app/src/main/java/com/example/instafameproj/ui/userprofile/UserProfileFragment.kt
@@ -7,12 +7,12 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.example.instafameproj.databinding.FragmentNotificationsBinding
+import com.example.instafameproj.databinding.FragmentUserBinding
 import androidx.lifecycle.Observer
 
 class UserProfileFragment : Fragment() {
 
-    private var _binding: FragmentNotificationsBinding? = null
+    private var _binding: FragmentUserBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -31,7 +31,7 @@ class UserProfileFragment : Fragment() {
             "23oifajsd;fkj20392r30000"
         )
 
-        _binding = FragmentNotificationsBinding.inflate(inflater, container, false)
+        _binding = FragmentUserBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
         userProfileViewModel.userIdLabel.observe(viewLifecycleOwner) {
